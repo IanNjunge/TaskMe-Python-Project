3f62f60ead4f2d54f5c7f502826fafb1d039f09b
IanNjunge <ian.njunge@student.moringaschool.com>
2025-08-27 19:42:01 +0300

    Saves changes

--- cli.py
+++ cli.py
@@ -7,3 +7,16 @@ session = SessionLocal()
 
 def menu():
     print("TaskMe")
+    print("1. New Task")
+    print("2. My Tasks")
+    print("3. Update Task Status")
+    print("4. Delete Task")
+    print("5. Exit TaskMe")
+    
+def add_task():
+    title = input("Task title: ")    
+    description = input("Describe task: ")
+    due = input("Due date (YYYY-MM-DD): ")
+    
+    category_name = input("Category: ")
+    category = session.query(Category).filter_by(name=category_name).first()
