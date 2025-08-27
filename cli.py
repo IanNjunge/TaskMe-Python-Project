f3634763ebfcd4864ddbbc79d9c673ee2381721c
IanNjunge <ian.njunge@student.moringaschool.com>
2025-08-27 20:49:42 +0300

    Saves changes to cli.py

--- cli.py
+++ cli.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 session = SessionLocal()
 
+#menu
 def menu():
     print("TaskMe")
     print("1. New Task")
@@ -14,18 +15,35 @@ def menu():
     print("5. Exit TaskMe")
     
 def add_task():
-    title = input("Task title: ")    
+    name = input("Task name: ")    
     description = input("Describe task: ")
     due = input("Due date (YYYY-MM-DD): ")
     
     category_name = input("Category: ")
     category = session.query(Category).filter_by(name=category_name).first()
+    if not category:
+        category = Category(name=category_name)
+        session.add(category)
+        
+        
+    task = Task(
+        name = name,
+        description = description,
+        due = datetime.striptime(due, "%Y-%m-%d"),
+        category = category)
     
-    task = Task()
     session.add(task)
     session.commit()
     print("Task added!")
     
 def view_tasks():
     tasks = session.query(Task).all()
+    if not tasks:
+        print("No tasks found.")
+        #return
+    #for t in tasks:
+         #print(f"[{t.id}] - {t.name} - {t.status} - {t.category.name} - (Due{t.due})") 
         
+    
+    
+     
